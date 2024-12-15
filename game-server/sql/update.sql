16e4dcfaffa13f6a36332b1c8270d735a78a211b
Neon <neon@beyond-aion.com>
2024-12-15 23:44:47 +0100

    Solorius Event 2024

--- game-server/sql/update.sql
+++ game-server/sql/update.sql
@@ -1,17 +1,6 @@
 /*
-* DB changes since 9556a4b (30.11.2024)
+* DB changes since 183e689 (08.12.2024)
  */
 
-ALTER TABLE `legion_history` DROP COLUMN `tab_id`;
-
--- drop advent calendar table
-drop table advent;
-
--- create advent calendar table
-CREATE TABLE `advent` (
-												`account_id` int(11) NOT NULL,
-												`last_day_received` tinyint(4) NOT NULL,
-												PRIMARY KEY (`account_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `player_veteran_rewards` CHANGE COLUMN `received_months` `received_months` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `player_id`;
+-- remove old event items
+DELETE FROM inventory WHERE item_id IN (182007148, 186000177, 186000381, 188052986, 188052987, 188052988, 188052989, 188052990, 188052991, 188052992, 188052993, 188052994, 188052995, 188052996, 188052997, 188052998, 188052999, 188100148, 188100149);
