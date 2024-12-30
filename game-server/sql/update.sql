343f10d5bdd655dd55d3a9590ed98d9b6fafb68b
Neon <neon@beyond-aion.com>
2024-12-30 22:32:19 +0100

    Optimized size of inventory table

--- game-server/sql/update.sql
+++ game-server/sql/update.sql
@@ -5,6 +5,11 @@
 DROP TABLE `skill_motions`;
 DROP TABLE `tasks`;
 DELETE inventory FROM inventory LEFT JOIN legions l ON l.id = item_owner WHERE item_location = 3 AND l.id IS NULL;
+ALTER TABLE `inventory`
+	CHANGE COLUMN `enchant` `enchant` TINYINT UNSIGNED NOT NULL DEFAULT '0' AFTER `item_location`,
+	CHANGE COLUMN `optional_socket` `optional_socket` TINYINT UNSIGNED NOT NULL DEFAULT (0) AFTER `fusioned_item`,
+	CHANGE COLUMN `optional_fusion_socket` `optional_fusion_socket` TINYINT UNSIGNED NOT NULL DEFAULT (0) AFTER `optional_socket`,
+	CHANGE COLUMN `rnd_plume_bonus` `rnd_plume_bonus` SMALLINT UNSIGNED NOT NULL DEFAULT (0) AFTER `buff_skill`;
 ALTER TABLE `player_registered_items`
 	DROP INDEX `item_unique_id`,
 	ADD UNIQUE INDEX `item_unique_id` (`item_unique_id`) USING BTREE;
